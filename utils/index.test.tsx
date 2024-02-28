edddd23f84a834c49d4479e037fbf60ab3e01e5b
divine <divineobi07@gmail.com>
2024-02-28 17:53:03 +0100

    test: add utils unit test

---
+++ utils/index.test.tsx
@@ -0,0 +1,56 @@
+//@ts-nocheck
+import { convertSecToStandardVideoDate, setup } from ".";
+
+describe("convertSecToStandardVideoDate", () => {
+  test("should return 0:00 when totalSecs is not a number or less than equal to zero", () => {
+    const totalSecs = null;
+    const totalSecs1 = false;
+    const totalSecs2 = 0;
+
+    expect(convertSecToStandardVideoDate(totalSecs)).toBe("0:00");
+    expect(convertSecToStandardVideoDate(totalSecs1)).toBe("0:00");
+    expect(convertSecToStandardVideoDate(totalSecs2)).toBe("0:00");
+  });
+
+  test("should return 1:00 and 0:56 when totalSecs is 60secs and 56s", () => {
+    const totalSecs = 60;
+    const totalSecs56 = 56;
+    expect(convertSecToStandardVideoDate(totalSecs)).toBe("1:00");
+    expect(convertSecToStandardVideoDate(totalSecs56)).toBe("0:56");
+  });
+
+  test("should return 24:00:00 and 23:59:59 when totalSecs is 86400secs and 86399 respectively", () => {
+    const totalSecs = 86400;
+    const totalSecs1 = 86399;
+    expect(convertSecToStandardVideoDate(totalSecs)).toBe("24:00:00");
+    expect(convertSecToStandardVideoDate(totalSecs1)).toBe("23:59:59");
+  });
+
+  test("should return 36:51 and 6:51 when totalsecs is 2211.23 and 411 respectively", () => {
+    const totalSecs = 2211.23;
+    const totalSecs1 = 411;
+    expect(convertSecToStandardVideoDate(totalSecs)).toBe("36:51");
+    expect(convertSecToStandardVideoDate(totalSecs1)).toBe("6:51");
+  });
+
+  test("should return 2:00:05 and 59:59 when totalsecs is 7205 and 3599 respectively", () => {
+    const totalSecs = 7205;
+    const totalSecs1 = 3599;
+    expect(convertSecToStandardVideoDate(totalSecs)).toBe("2:00:05");
+    expect(convertSecToStandardVideoDate(totalSecs1)).toBe("59:59");
+  });
+});
+
+describe("setup", () => {
+  test("should render jsx element properly ", () => {
+    const jsx = <div>hello divine</div>;
+
+
+    const result = setup(jsx);
+
+    expect(result).toHaveProperty("getByRole");
+    expect(result).toHaveProperty("user");
+    expect(result).toHaveProperty("findByRole");
+    expect(result).toHaveProperty("queryByRole");
+  });
+});
--- utils/index.ts
+++ utils/index.ts
@@ -3,7 +3,7 @@ import { userEvent } from "@testing-library/user-event";
 import { render } from "@testing-library/react";
 
 export const convertSecToStandardVideoDate = (totalSecs: number): string => {
-  if (!totalSecs) return "0:00";
+  if (!Number.isFinite(totalSecs) || totalSecs <= 0) return "0:00";
 
   // if totalSecs is over 3600 === render in hours format e.g 02:01:56
   if (totalSecs >= 3600) {
@@ -11,7 +11,6 @@ export const convertSecToStandardVideoDate = (totalSecs: number): string => {
     const rem_hrs = totalSecs % 3600;
     const mins = rem_hrs / 60;
     const secs = rem_hrs % 60;
-    // console.log(hrs, mins, secs, totalSecs);
 
     return `${Math.floor(hrs).toString()}:${Math.floor(mins)
       .toString()
@@ -28,22 +27,22 @@ export const convertSecToStandardVideoDate = (totalSecs: number): string => {
   return durationMaths;
 };
 
-export const rangeTimeFormat = (
-  currentTime: number,
-  duration: number
-): number | null => {
-  if (!currentTime && !duration) return null;
+// export const rangeTimeFormat = (
+//   currentTime: number,
+//   duration: number
+// ): number | null => {
+//   if (!currentTime && !duration) return null;
 
-  const value = (currentTime / duration) * 100;
-  return value;
-};
+//   const value = (currentTime / duration) * 100;
+//   return value;
+// };
 
 export interface VideoRefProps {
   videoRef: RefObject<HTMLVideoElement | null>;
 }
 
 // default for reusable user event
-// See https://testing-library.com/docs/dom-testing-library/install#wrappers
+// Reference - https://testing-library.com/docs/dom-testing-library/install#wrappers
 export function setup(jsx: JSX.Element) {
   return {
     user: userEvent.setup(),
